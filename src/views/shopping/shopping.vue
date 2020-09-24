4a8059a51e5a05a894fdf57c30f131d4ad288aca
青空 <1057605824@qq.com>
2020-09-24 21:31:28 +0800

    商品数据渲染‘

--- src/views/shopping/shopping.vue
+++ src/views/shopping/shopping.vue
@@ -1,15 +1,65 @@
 <template>
-  <div>
-      asdfkjsdh
-  </div>
+    <div>
+        <el-table ref="filterTable" :data="commodity" style="width: 100%">
+            <el-table-column
+                :prop="item.en"
+                :label="item.cn"
+                v-for="item in showItems"
+                :key="item.cn"
+            >
+            </el-table-column>
+        </el-table>
+    </div>
 </template>
 
 <script>
 export default {
-
-}
+    data() {
+        return {
+            commodity: [],
+            showItems: [
+                {
+                    cn: "商品id",
+                    en: "id",
+                },
+                {
+                    cn: "商品名称",
+                    en: "name",
+                },
+                {
+                    cn: "价格",
+                    en: "price",
+                },
+                {
+                    cn: "标签",
+                    en: "description",
+                },
+                {
+                    cn: "图片地址",
+                    en: "image",
+                },
+                {
+                    cn: "店铺名称",
+                    en: "store-name",
+                },
+                {
+                    cn: "位置",
+                    en: "position",
+                },
+            ],
+        };
+    },
+    methods: {
+        async getRecommend() {
+            const datas = await this.axios.get("/recommend");
+            this.commodity = datas.data;
+            console.log(datas);
+        },
+    },
+    created() {
+        this.getRecommend();
+    },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
